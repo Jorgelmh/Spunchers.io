55bf7bdfbeee426a527f75d85bb5fa65dc7dda12
Jorge <jorgelmh_01@hotmail.com>
2020-04-19 02:18:22 +0100

    Adding select skin feature

--- server/public/assets/js/online.js
+++ server/public/assets/js/online.js
@@ -6,15 +6,47 @@ import Glide from '@glidejs/glide'
  *  ============================
  */
 const socket = io()
-const url = new URL(window.location)
-let skin = url.searchParams.get('skin')
+let game
 
-console.log('hello world from js')
+const characterSkins = ['c3po', 'deadpool', 'captainamerica', 'mandalorian', 'nickfury', 'pirate', 'stormtrooper', 'tonystark']
+const track = document.getElementsByClassName('glide__slides')[0]
 
-if(!skin)
-    skin = 'stormtrooper'
+characterSkins.map((skin) => {
+    let divHTML = document.createElement('div')
+    divHTML.classList = "glide__slide single-character-skin"
+
+    let name = document.createElement('h1')
+    name.innerHTML = skin
+    name.className = 'character-name'
+
+    let imgHTML = document.createElement('img')
+    imgHTML.src = `../assets/selectCharacters/${skin}.png`
+
+    divHTML.append(name)
+    divHTML.append(imgHTML)
+    track.append(divHTML)
+})
+
+/* Creating the Glide object to show the available skins */
+
+const glide = new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1
+  }).mount()
+
+
+const play = document.getElementById('play')
+play.onclick = (event) => {
+    let body =document.getElementsByTagName('body')[0]
+    body.classList.remove('background-connect-frame')
+
+    body.removeChild(document.getElementById('login-frame'))
+    const engine = new Online(game.lobby.map, game.lobby.colissionMatrix, game.lobby.tileSet , document.getElementById('game'), socket, game.playerID, game.lobby.server, characterSkins[glide.index])
+}
+
 
 socket.on('loadMap', (data) => {
-    const game = new Online(data.lobby.map, data.lobby.colissionMatrix, data.lobby.tileSet , document.getElementById('game'), socket, data.playerID, data.lobby.server, skin)
+    game = data
 })
  
