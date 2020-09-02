57ce816d022635e923e54ede3fb09e604d881791
Jorge <jorgelmh_01@hotmail.com>
2020-09-02 15:19:41 +0200

    Checking previous state

--- server/client/js/classes/Online.js
+++ server/client/js/classes/Online.js
@@ -53,10 +53,9 @@ export default class Online extends Engine{
             if(currentPlayerPos){
                 let startPoints = this.calculateLocalMap(currentPlayerPos.posX, currentPlayerPos.posY)
 
-                setTimeout(() => {
                     this.tileMap.startX = startPoints.posX
                     this.tileMap.startY = startPoints.posY
-                }, 150)
+                
                 
             }
             
