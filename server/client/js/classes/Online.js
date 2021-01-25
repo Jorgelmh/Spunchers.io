558cc5eab011ed52c0ebb8d473f94154298844f3
Jorge <jorgelmh_01@hotmail.com>
2021-01-25 01:29:51 +0100

    Last delay 2

--- server/client/js/classes/Online.js
+++ server/client/js/classes/Online.js
@@ -53,7 +53,7 @@ export default class Online extends Engine{
         this.lastInterpolation = 0
 
         /* Constant of interpolation delay */
-        this.interpolationDelay = 18
+        this.interpolationDelay = 19
         
         /* SOCKET LISTENERS */
         this.socketIO.on('state', (data) =>{
